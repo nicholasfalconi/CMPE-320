e292f0fc56917f552fca46b3878bf52cf9382460
Chris Maltais <chris.maltais@hotmail.ca>
2018-09-12 20:18:13 -0400

    Figure out header files/Generate and Save

--- AssignmentOne/insultgenerator_14cm24.cpp
+++ AssignmentOne/insultgenerator_14cm24.cpp
@@ -2,45 +2,80 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
-
+#include <set>
+#include <iterator>
+#include <time.h>
+#include "insultgenerator_14cm24.h"
 using namespace std;
 
-class InsultGenerator {
-	private:
-		vector <string> firstColumnWords;
-		vector <string> secondColumnWords;
-		vector <string> thirdColumnWords;
-		string getRandomWord(vector<string> words) {
-			srand(time(0));
-			return words[rand() % words.size()];
-		}
-
-	public:
-		void initialize() {
-			ifstream inputFile("InsultsSource.txt");
-			string oneLine;
-			while ( getline(inputFile, oneLine, '\n') ) {
-				vector <string> currentLineWords;
-				stringstream ss(oneLine);
-			    string word;
-			    while (getline(ss, word, '\t')) {
-			    	currentLineWords.push_back(word);
-			    }
-			    firstColumnWords.push_back(currentLineWords[0]);
-			    secondColumnWords.push_back(currentLineWords[1]);
-			    thirdColumnWords.push_back(currentLineWords[2]);
-			}
-		}
-
-		string talkToMe() {
-			return getRandomWord(firstColumnWords) + " " + getRandomWord(secondColumnWords) + " " + getRandomWord(thirdColumnWords);
-		}
-
-
+InsultGenerator::InsultGenerator() {
+	cout << "InsultGenerator class instantiated." << endl;
 };
+
+string InsultGenerator::getRandomWord(vector<string> words) {
+	return words[rand() % words.size()];
+}
+
+
+void InsultGenerator::initialize() {
+	ifstream inputFile("InsultsSource.txt");
+	string oneLine;
+	while ( getline(inputFile, oneLine, '\n') ) {
+		vector <string> currentLineWords;
+		stringstream ss(oneLine);
+		string word;
+		while (getline(ss, word, '\t')) {
+			currentLineWords.push_back(word);
+		}
+		this->firstColumnWords.push_back(currentLineWords[0]);
+		this->secondColumnWords.push_back(currentLineWords[1]);
+		this->thirdColumnWords.push_back(currentLineWords[2]);
+	}
+}
+
+// O(1)
+string InsultGenerator::talkToMe() {
+	return getRandomWord(firstColumnWords) + " " + getRandomWord(secondColumnWords) + " " + getRandomWord(thirdColumnWords);
+}
+
+// O(numInsults * nlogn)
+vector <string> InsultGenerator::generate(int numInsults) {
+	vector<string> allInsults;
+	set<string> mySet;
+	string insult;
+	pair<set<string>::iterator, bool> ret;
+	srand(time(NULL));
+	for (int i = 0; i < numInsults; i++) {
+		insult = talkToMe();
+		ret = mySet.insert(insult); // O(logn)
+		bool isUnique(ret.second); // O(1)
+		while (!isUnique) {
+			insult = talkToMe();
+			ret = mySet.insert(insult);
+			isUnique = ret.second;
+		}
+	}
+	allInsults.assign(mySet.begin(), mySet.end()); // O(n)
+	return allInsults;
+}
+
+void InsultGenerator::generateAndSave(string fileName, int numInsults) {
+	vector<string> allInsults = generate(numInsults);
+	ofstream outFile(fileName);
+	for (auto insult : allInsults) {
+		outFile << insult << endl;
+	}
+}
+
 int main() {
 	InsultGenerator ig;
 	ig.initialize();
-	cout << ig.talkToMe() << endl;
+	vector<string> insults;
+	double start, finish;
+	start = clock();
+	insults = ig.generate(10000);
+	finish = clock();
+	ig.generateAndSave("test.txt", 100);
+	cout << (finish - start) << " ms to generate insults" << endl;
 	return 0;
 }
--- AssignmentOne/insultgenerator_14cm24.h
+++ AssignmentOne/insultgenerator_14cm24.h
@@ -5,15 +5,30 @@
  *      Author: chrismaltais
  */
 
-#ifndef INSULTGENERATOR_14CM24_H
-#define INSULTGENERATOR_14CM24_H
+// Include Guards
+#ifndef INSULTGENERATOR_14CM24_H_
+#define INSULTGENERATOR_14CM24_H_
 
-class InsultGenerator() {
+// Dependencies
+#include <vector>
+#include <exception>
+
+class InsultGenerator {
+	private:
+		std::vector<std::string> firstColumnWords;
+		std::vector<std::string> secondColumnWords;
+		std::vector<std::string> thirdColumnWords;
+		std::string getRandomWord(std::vector<std::string> words);
 	public:
 		InsultGenerator();
+		void initialize();
+		std::string talkToMe();
+		std::vector<std::string> generate(int numInsults);
+		void generateAndSave(std::string fileName, int numInsults);
 };
 
+class FileException : public exception {
 
-
+};
 
 #endif /* INSULTGENERATOR_14CM24_H_ */
