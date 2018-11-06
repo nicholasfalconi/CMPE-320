935586d491330f74d1ea310c6f6089ee3de51de1
Chris Maltais <14cm24@queensu.ca>
2018-11-06 13:26:54 -0500

    Add Completed Assignment3

--- Assignment3/Fraction_DLL2/GUI_Part/GUI.h
+++ Assignment3/Fraction_DLL2/GUI_Part/GUI.h
@@ -63,6 +63,12 @@ namespace GUIPart {
 	private: System::Windows::Forms::Label^  label2;
 	private: System::Windows::Forms::Label^  label3;
 	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::Label^  label7;
+	private: System::Windows::Forms::Label^  label8;
+	private: System::Windows::Forms::Label^  label9;
+	private: System::Windows::Forms::Label^  label10;
 
 	private:
 		/// <summary>
@@ -92,6 +98,12 @@ namespace GUIPart {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// num1TB
@@ -100,6 +112,8 @@ namespace GUIPart {
 			this->num1TB->Name = L"num1TB";
 			this->num1TB->Size = System::Drawing::Size(72, 20);
 			this->num1TB->TabIndex = 0;
+			this->num1TB->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->num1TB->TextChanged += gcnew System::EventHandler(this, &GUI::num1TB_TextChanged);
 			// 
 			// denom1TB
 			// 
@@ -107,36 +121,46 @@ namespace GUIPart {
 			this->denom1TB->Name = L"denom1TB";
 			this->denom1TB->Size = System::Drawing::Size(72, 20);
 			this->denom1TB->TabIndex = 1;
+			this->denom1TB->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->denom1TB->TextChanged += gcnew System::EventHandler(this, &GUI::denom1TB_TextChanged);
 			// 
 			// num2TB
 			// 
 			this->num2TB->Location = System::Drawing::Point(166, 41);
 			this->num2TB->Name = L"num2TB";
 			this->num2TB->Size = System::Drawing::Size(72, 20);
-			this->num2TB->TabIndex = 7;
+			this->num2TB->TabIndex = 3;
+			this->num2TB->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->num2TB->TextChanged += gcnew System::EventHandler(this, &GUI::num2TB_TextChanged);
 			// 
 			// denom2TB
 			// 
 			this->denom2TB->Location = System::Drawing::Point(166, 109);
 			this->denom2TB->Name = L"denom2TB";
 			this->denom2TB->Size = System::Drawing::Size(72, 20);
-			this->denom2TB->TabIndex = 8;
+			this->denom2TB->TabIndex = 4;
+			this->denom2TB->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->denom2TB->TextChanged += gcnew System::EventHandler(this, &GUI::denom2TB_TextChanged);
 			// 
 			// denomAnsTB
 			// 
+			this->denomAnsTB->Enabled = false;
 			this->denomAnsTB->Location = System::Drawing::Point(311, 109);
 			this->denomAnsTB->Name = L"denomAnsTB";
 			this->denomAnsTB->Size = System::Drawing::Size(72, 20);
 			this->denomAnsTB->TabIndex = 4;
 			this->denomAnsTB->TabStop = false;
+			this->denomAnsTB->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
 			// numAnsTB
 			// 
+			this->numAnsTB->Enabled = false;
 			this->numAnsTB->Location = System::Drawing::Point(311, 41);
 			this->numAnsTB->Name = L"numAnsTB";
 			this->numAnsTB->Size = System::Drawing::Size(72, 20);
 			this->numAnsTB->TabIndex = 5;
 			this->numAnsTB->TabStop = false;
+			this->numAnsTB->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
 			// addCB
 			// 
@@ -144,7 +168,7 @@ namespace GUIPart {
 			this->addCB->Location = System::Drawing::Point(128, 43);
 			this->addCB->Name = L"addCB";
 			this->addCB->Size = System::Drawing::Size(32, 17);
-			this->addCB->TabIndex = 3;
+			this->addCB->TabIndex = 2;
 			this->addCB->Text = L"+";
 			this->addCB->UseVisualStyleBackColor = true;
 			this->addCB->CheckedChanged += gcnew System::EventHandler(this, &GUI::addCB_CheckedChanged);
@@ -156,6 +180,7 @@ namespace GUIPart {
 			this->subCB->Name = L"subCB";
 			this->subCB->Size = System::Drawing::Size(29, 17);
 			this->subCB->TabIndex = 4;
+			this->subCB->TabStop = false;
 			this->subCB->Text = L"-";
 			this->subCB->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			this->subCB->UseVisualStyleBackColor = true;
@@ -168,6 +193,7 @@ namespace GUIPart {
 			this->mulCB->Name = L"mulCB";
 			this->mulCB->Size = System::Drawing::Size(31, 17);
 			this->mulCB->TabIndex = 5;
+			this->mulCB->TabStop = false;
 			this->mulCB->Text = L"x";
 			this->mulCB->UseVisualStyleBackColor = true;
 			this->mulCB->CheckedChanged += gcnew System::EventHandler(this, &GUI::mulCB_CheckedChanged);
@@ -179,6 +205,7 @@ namespace GUIPart {
 			this->divCB->Name = L"divCB";
 			this->divCB->Size = System::Drawing::Size(31, 17);
 			this->divCB->TabIndex = 6;
+			this->divCB->TabStop = false;
 			this->divCB->Text = L"/";
 			this->divCB->UseVisualStyleBackColor = true;
 			this->divCB->CheckedChanged += gcnew System::EventHandler(this, &GUI::divCB_CheckedChanged);
@@ -228,11 +255,72 @@ namespace GUIPart {
 			this->label4->TabIndex = 14;
 			this->label4->Text = L"-------------------";
 			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(37, 144);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(103, 13);
+			this->label5->TabIndex = 15;
+			this->label5->Text = L"Keyboard Shortcuts:";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(38, 157);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(125, 13);
+			this->label6->TabIndex = 16;
+			this->label6->Text = L"Close Application: Alt + C";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(37, 170);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(160, 13);
+			this->label7->TabIndex = 17;
+			this->label7->Text = L"Toggle Addition: Alt + Shift + \"=\"";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(37, 183);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(140, 13);
+			this->label8->TabIndex = 18;
+			this->label8->Text = L"Toggle Subtraction: Alt + \"-\"";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(38, 196);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(173, 13);
+			this->label9->TabIndex = 19;
+			this->label9->Text = L"Toggle Multiplication: Alt + Shift + 8";
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(38, 209);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(125, 13);
+			this->label10->TabIndex = 20;
+			this->label10->Text = L"Toggle Division: Alt + \"/\"";
+			// 
 			// GUI
 			// 
+			this->AcceptButton = this->equalsBut;
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(426, 225);
+			this->ClientSize = System::Drawing::Size(426, 237);
+			this->Controls->Add(this->label10);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
@@ -248,9 +336,11 @@ namespace GUIPart {
 			this->Controls->Add(this->num2TB);
 			this->Controls->Add(this->denom1TB);
 			this->Controls->Add(this->num1TB);
+			this->KeyPreview = true;
 			this->Name = L"GUI";
 			this->Text = L"Fraction Calculator by Chris Maltais";
 			this->Load += gcnew System::EventHandler(this, &GUI::GUI_Load);
+			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &GUI::keyDownEvent);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -264,6 +354,28 @@ namespace GUIPart {
 		// Close button should close window (close())
 		// On Change of textboxes, all values should be wiped
 	}
+
+private: System::Void keyDownEvent(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+	if (e->Alt && e->KeyCode == Keys::C) {
+		this->Close();
+	}
+	if (e->Alt && e->Shift && e->KeyCode == Keys::Oemplus) {
+		addCB->Checked = true;
+	}
+	if (e->Alt && e->KeyCode == Keys::OemMinus) {
+		subCB->Checked = true;
+	}
+	if (e->Alt && e->Shift && e->KeyCode == Keys::D8) {
+		mulCB->Checked = true;
+	}
+	if (e->Alt && e->KeyCode == Keys::OemQuestion) {
+		divCB->Checked = true;
+	}
+	if (e->KeyCode == Keys::Enter) {
+		this->Close();
+	}
+	
+}
 
 private: System::Void addCB_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
 	if (addCB->Checked) {
@@ -365,10 +477,6 @@ private: System::Void equalsBut_Click(System::Object^  sender, System::EventArgs
 	}
 
 	int* ans;
-	cout << num1 << endl;
-	cout << denom1 << endl;
-	cout << num2 << endl;
-	cout << denom2 << endl;
 
 	if (addCB->Checked) {
 		ans = addFrac(num1, denom1, num2, denom2);
@@ -381,9 +489,29 @@ private: System::Void equalsBut_Click(System::Object^  sender, System::EventArgs
 		ans = divFrac(num1, denom1, num2, denom2);
 	}
 
-	cout << ans[0] << "/" << ans[1] << endl;
+	numAnsTB->Text = Convert::ToString(ans[0]);
+	denomAnsTB->Text = Convert::ToString(ans[1]);
 
 	// If result is negative, only numerator negative
 }
+
+// On textbox change, wipe answer
+private: System::Void denom2TB_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	numAnsTB->Text = "";
+	denomAnsTB->Text = "";
+}
+private: System::Void num2TB_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	numAnsTB->Text = "";
+	denomAnsTB->Text = "";
+}
+private: System::Void denom1TB_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	numAnsTB->Text = "";
+	denomAnsTB->Text = "";
+}
+private: System::Void num1TB_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	numAnsTB->Text = "";
+	denomAnsTB->Text = "";
+}
+
 };
 }
